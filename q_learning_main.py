ff32ded7932577677879690b451c89fd2fa5bc7e
kostaskracht <kkrachtop@gmail.com>
2021-12-18 13:11:42 +0100

    Performed mostly formatting updates.

--- q_learning_main.py
+++ q_learning_main.py
@@ -2,8 +2,11 @@ import simple_grid
 from q_learning_skeleton import *
 import gym
 
+
 def act_loop(env, agent, num_episodes):
+    policy = []
     for episode in range(num_episodes):
+        episode_policy = []
         state = env.reset()
         agent.reset_episode()
 
@@ -15,6 +18,7 @@ def act_loop(env, agent, num_episodes):
         for t in range(MAX_EPISODE_LENGTH):
             if renderit:
                 env.render()
+                print("state:", state)
             printing=False
             if t % 500 == 499:
                 printing = True
@@ -22,31 +26,35 @@ def act_loop(env, agent, num_episodes):
             if printing:
                 print('---stage %d---' % t)
                 agent.report()
-                print("state:", state)
 
             action = agent.select_action(state)
-            if episode == 999:
-                print(action)
+            episode_policy.append(action)
             new_state, reward, done, info = env.step(action)
             if printing:
                 print("act:", action)
+                print("new state", state)
                 print("reward=%s" % reward)
 
+            # Update the Q function for the new state
             agent.process_experience(state, action, new_state, reward, done)
+            # Update the state
             state = new_state
             if done:
+                policy.append(episode_policy)
                 print("Episode finished after {} timesteps".format(t+1))
                 env.render()
                 agent.report()
                 break
-    print(agent.Q)
-    print(agent.epsilon)
-    for i in range(len(agent.Q)):
-        print(np.argmax(agent.Q[i,:]))
-    env.close()
+            if t == MAX_EPISODE_LENGTH - 1:
+                policy.append(episode_policy)
 
+    env.close()
+    # optionally save the policies created
+    #np.save("policy_alley_decay_deprpen_5000", np.array(policy))
+    #np.save("q_values_alley_decay_deprpen_5000", agent.Q)
 
 if __name__ == "__main__":
+    # TODO - comment in/out based on which environment you want to run
     #env = simple_grid.DrunkenWalkEnv(map_name="walkInThePark")
     env = simple_grid.DrunkenWalkEnv(map_name="theAlley")
     num_a = env.action_space.n
@@ -55,7 +63,6 @@ if __name__ == "__main__":
         num_o = env.observation_space.n
     else:
         raise("Qtable only works for discrete observations")
-
 
     discount = DEFAULT_DISCOUNT
     ql = QLearner(num_o, num_a, discount) #<- QTable
--- q_learning_skeleton.py
+++ q_learning_skeleton.py
@@ -1,16 +1,18 @@
 import random
 import numpy as np
 
-NUM_EPISODES = 100000
-MAX_EPISODE_LENGTH = 1000
-
+NUM_EPISODES = 1000
+MAX_EPISODE_LENGTH = 500
 
 DEFAULT_DISCOUNT = 0.9
-EPSILON_MAX = 1
+EPSILON = 0.05
 LEARNINGRATE = 0.1
+
+# Coding exercise 2 (decaying epsilon)
+DECAY_ENABLED = True
+EPSILON_MAX = 1
 EPSILON_MIN = 0.05
-EPSILON = 1
-EPSILON_DECAY = 0.9999
+EPSILON_DECAY = 1 - 1/NUM_EPISODES
 
 
 class QLearner():
@@ -20,16 +22,22 @@ class QLearner():
     def __init__(self, num_states, num_actions, discount=DEFAULT_DISCOUNT, learning_rate=LEARNINGRATE): 
         self.name = "agent1"
         self.Q = np.zeros((num_states, num_actions))
-        self.epsilon = EPSILON_MAX
+        if DECAY_ENABLED:
+            self.epsilon = EPSILON_MAX
+        else:
+            self.epsilon = EPSILON
 
 
     def reset_episode(self):
         """
         Here you can update some of the statistics that could be helpful to maintain
         """
-        self.epsilon = EPSILON_DECAY*self.epsilon
-        #if self.epsilon < 0.01:
-            #self.epsilon = 0.01
+        if DECAY_ENABLED:
+            self.epsilon = EPSILON_DECAY*self.epsilon
+            if self.epsilon < 0.01:
+                self.epsilon = 0.01
+        else:
+            pass
 
 
     def process_experience(self, state, action, next_state, reward, done): 
@@ -41,18 +49,18 @@ class QLearner():
         else:
             self.Q[state, action] = (1 - LEARNINGRATE)*self.Q[state, action] + LEARNINGRATE*reward
 
-
     def select_action(self, state): 
         """
         Returns an action, selected based on the current state
         """
-        epsilon = random.random()
+        epsilon = random.random() # return probability between [0.0, 1.0)
         if epsilon < self.epsilon:
+            # Exploration: If probability is smaller than EPSILON, pick one action at random
             return random.randint(0, 3)
         else:
+            # Exploitation: If probability is larger than EPSILON, pick the action with the
+            # highest q value
             return np.random.choice(np.argwhere(self.Q[state,:] == np.max(self.Q[state, :])).flatten().tolist())
-
-
 
     def report(self):
         """
